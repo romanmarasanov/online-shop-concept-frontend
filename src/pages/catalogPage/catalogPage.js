f0c3ffe8576139f05a94dff15cd19e1cdbb16cd4
RomanMaras <romamar2000@gmail.com>
2024-03-31 10:16:48 +0300

    add footer to catalog page

--- src/pages/catalogPage/catalogPage.js
+++ src/pages/catalogPage/catalogPage.js
@@ -1,11 +1,13 @@
 
 import { SiteCeiling } from '../../components/ceiling/siteCeiling';
+import { SiteFooter } from '../../components/footer/siteFooter';
 
 export const CatalogPage = () => {
 	return (
 		<div>
 			<SiteCeiling />
 			Catalog page
+			<SiteFooter />
 		</div>
 	);
 };
